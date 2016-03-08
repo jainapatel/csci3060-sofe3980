cee0893c4e7036dd5f813643f460137b2baef6ff
jainapatel <jaina.patel@uoit.net>
2016-03-08 00:05:21 -0500

    Update utilities.h

--- project-phase2/src/utilities.h
+++ project-phase2/src/utilities.h
@@ -1,14 +1,14 @@
 /*
 authors: Khirthana Subramanian - 100453865
          Jaina Patel - 100523188
-				 James Morrison - 100524362
+		 James Morrison - 100524362
 */
 
 #include <stdio.h>
 #include "account.h"
 #include <fstream>
 #include <regex>
-#include <list>
+#include <vector>
 #include <iostream>
 #include <string>
 
@@ -18,10 +18,10 @@ authors: Khirthana Subramanian - 100453865
 
 class Utilities{
 public:
-	static std::string Prompt(std::string output, std::string reg = "", std::list<std::string> acceptions = {});
-	static std::list<Account> LoadAccountInformation(std::string accounts_file);
-	static Account *GetAccountFromName(std::string name, std::list<Account> accounts);
-	static Account *GetAccountFromNumber(std::string number, std::list<Account> accounts);
+	static std::string Prompt(std::string output, std::string reg = "");
+	static std::vector<Account> LoadAccountInformation(std::string accounts_file);
+	static Account *GetAccountFromName(std::string name, std::vector<Account> accounts);
+	static Account *GetAccountFromNumber(std::string number, std::vector<Account> accounts);
 };
 
 #endif
